d21f985bdd7386c6b1fcdb1ca51147c70d1d9dbe
Sophie Guile <Sophie.Guile@softwire.com>
2022-04-27 15:40:51 +0100

    Knights can now move

--- src/engine/pieces/knight.ts
+++ src/engine/pieces/knight.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Board from "../board";
 import Player from "../player";
+import Square from "../square";
 
 export default class Knight extends Piece {
     constructor(player: Player) {
@@ -8,6 +9,30 @@ export default class Knight extends Piece {
     }
 
     getAvailableMoves(board: Board) {
-        return new Array(0);
+
+        const moves: Square[] = new Array(0);
+        const currentSquare: Square = board.findPiece(this);
+try {
+    
+}
+        for(let rowDelta = -2; rowDelta <= 2; rowDelta++){
+            for(let colDelta = -2; colDelta <= 2; colDelta++){
+
+                //one should be size 1 and one should be size 2 (total move size of 3 steps)
+                if(Math.abs(rowDelta) + Math.abs(colDelta) !== 3){
+                    continue;
+                }
+
+                const destinationSquare = new Square(currentSquare.row + rowDelta, currentSquare.col + colDelta);
+
+                if(board.isInBoard(destinationSquare)){
+                    moves.push(destinationSquare);
+                }
+            }
+        }
+
+        return moves;
     }
+
+
 }
